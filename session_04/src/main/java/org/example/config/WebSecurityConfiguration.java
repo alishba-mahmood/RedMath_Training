0179895692de2c7bff9ec2cbc507036f7da6a9a1
DESKTOP-13NIND8\alishba.mahmood <alishbamahmood1311@gmail.com>
2023-08-25 10:50:09 +0500

    session-04

---
+++ session_04/src/main/java/org/example/config/WebSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package org.example.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableMethodSecurity
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/api/v1/news","GET"));
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(Customizer.withDefaults());
+        http.authorizeHttpRequests(config -> config.anyRequest().authenticated());
+        return http.build();
+    }
+}
---
+++ session_04/src/main/java/org/example/core/NewsController.java
@@ -0,0 +1,68 @@
+package org.example.core;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import ch.qos.logback.core.pattern.parser.OptionTokenizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/news")
+public class NewsController {
+
+    private final NewsService service;
+
+    public NewsController(NewsService service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<News>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<News> findById(@PathVariable("id") Long id) {
+        Optional<News> news = service.findById(id);
+        if (news.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(news.get());
+    }
+
+    @PreAuthorize("hasAuthority('REPORTER')")
+    @PostMapping()
+    public ResponseEntity<News> create(@RequestBody News news) {
+        News created = service.create(news);
+        if (created != null) {
+            return ResponseEntity.ok(created);
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @PreAuthorize("hasAuthority('EDITOR')")
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody News news) {
+        Optional<News> newsFound = service.findById(id);
+        if (newsFound.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        service.update(id,news);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PreAuthorize("hasAuthority('EDITOR')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<News> news = service.findById(id);
+        if (news.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        News news1 = news.get();
+        service.delete(news1);
+        return ResponseEntity.noContent().build();
+    }
+}
